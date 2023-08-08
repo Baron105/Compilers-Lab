493374d54d14a01810377c2be0bb0cdf79ac67eb
root <root@HP-Pavillion.localdomain>
2023-08-08 23:17:40 +0530

    using linux to generate the .s file

--- Assignment 1/ass1_21CS10014.s
+++ Assignment 1/ass1_21CS10014.s
@@ -1,125 +1,114 @@
 	.file	"code.c"
 	.text
-	.def	scanf;	.scl	3;	.type	32;	.endef
-	.seh_proc	scanf
-scanf:
-	pushq	%rbp
-	.seh_pushreg	%rbp
-	pushq	%rbx
-	.seh_pushreg	%rbx
-	subq	$56, %rsp
-	.seh_stackalloc	56
-	leaq	48(%rsp), %rbp
-	.seh_setframe	%rbp, 48
-	.seh_endprologue
-	movq	%rcx, 32(%rbp)
-	movq	%rdx, 40(%rbp)
-	movq	%r8, 48(%rbp)
-	movq	%r9, 56(%rbp)
-	leaq	40(%rbp), %rax
-	movq	%rax, -16(%rbp)
-	movq	-16(%rbp), %rbx
-	movl	$0, %ecx
-	movq	__imp___acrt_iob_func(%rip), %rax
-	call	*%rax
-	movq	%rbx, %r8
-	movq	32(%rbp), %rdx
-	movq	%rax, %rcx
-	call	__mingw_vfscanf
-	movl	%eax, -4(%rbp)
-	movl	-4(%rbp), %eax
-	addq	$56, %rsp
-	popq	%rbx
-	popq	%rbp
-	ret
-	.seh_endproc
-	.def	printf;	.scl	3;	.type	32;	.endef
-	.seh_proc	printf
-printf:
-	pushq	%rbp
-	.seh_pushreg	%rbp
-	pushq	%rbx
-	.seh_pushreg	%rbx
-	subq	$56, %rsp
-	.seh_stackalloc	56
-	leaq	48(%rsp), %rbp
-	.seh_setframe	%rbp, 48
-	.seh_endprologue
-	movq	%rcx, 32(%rbp)
-	movq	%rdx, 40(%rbp)
-	movq	%r8, 48(%rbp)
-	movq	%r9, 56(%rbp)
-	leaq	40(%rbp), %rax
-	movq	%rax, -16(%rbp)
-	movq	-16(%rbp), %rbx
-	movl	$1, %ecx
-	movq	__imp___acrt_iob_func(%rip), %rax
-	call	*%rax
-	movq	%rbx, %r8
-	movq	32(%rbp), %rdx
-	movq	%rax, %rcx
-	call	__mingw_vfprintf
-	movl	%eax, -4(%rbp)
-	movl	-4(%rbp), %eax
-	addq	$56, %rsp
-	popq	%rbx
-	popq	%rbp
-	ret
-	.seh_endproc
 	.globl	calculateFrequency
-	.def	calculateFrequency;	.scl	2;	.type	32;	.endef
-	.seh_proc	calculateFrequency
+	.type	calculateFrequency, @function
 calculateFrequency:
+.LFB0:
+	.cfi_startproc
+	endbr64
 	pushq	%rbp
-	.seh_pushreg	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
 	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
-	subq	$16, %rsp
-	.seh_stackalloc	16
-	.seh_endprologue
-	movq	%rcx, 16(%rbp)
-	movl	%edx, 24(%rbp)
-	movq	%r8, 32(%rbp)
-	movl	$0, -4(%rbp)
-	jmp	.L6
-.L11:
-	movl	$1, -12(%rbp)
-	movl	-4(%rbp), %eax
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movl	%esi, -28(%rbp)
+	movq	%rdx, -40(%rbp)
+	movl	$0, -12(%rbp)
+	jmp	.L2
+.L7:
+	movl	$1, -4(%rbp)
+	movl	-12(%rbp), %eax
 	addl	$1, %eax
 	movl	%eax, -8(%rbp)
-	jmp	.L7
-.L9:
-	movl	-4(%rbp), %eax
+	jmp	.L3
+.L5:
+	movl	-12(%rbp), %eax
 	cltq
 	leaq	0(,%rax,4), %rdx
-	movq	16(%rbp), %rax
+	movq	-24(%rbp), %rax
 	addq	%rdx, %rax
 	movl	(%rax), %edx
 	movl	-8(%rbp), %eax
 	cltq
 	leaq	0(,%rax,4), %rcx
-	movq	16(%rbp), %rax
+	movq	-24(%rbp), %rax
 	addq	%rcx, %rax
 	movl	(%rax), %eax
 	cmpl	%eax, %edx
-	jne	.L8
-	addl	$1, -12(%rbp)
+	jne	.L4
+	addl	$1, -4(%rbp)
 	movl	-8(%rbp), %eax
 	cltq
 	leaq	0(,%rax,4), %rdx
-	movq	32(%rbp), %rax
+	movq	-40(%rbp), %rax
 	addq	%rdx, %rax
 	movl	$0, (%rax)
-.L8:
+.L4:
 	addl	$1, -8(%rbp)
-.L7:
+.L3:
 	movl	-8(%rbp), %eax
-	cmpl	24(%rbp), %eax
-	jl	.L9
+	cmpl	-28(%rbp), %eax
+	jl	.L5
+	movl	-12(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-40(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	testl	%eax, %eax
+	je	.L6
+	movl	-12(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-40(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-4(%rbp), %eax
+	movl	%eax, (%rdx)
+.L6:
+	addl	$1, -12(%rbp)
+.L2:
+	movl	-12(%rbp), %eax
+	cmpl	-28(%rbp), %eax
+	jl	.L7
+	nop
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	calculateFrequency, .-calculateFrequency
+	.section	.rodata
+.LC0:
+	.string	"Element\tFrequency"
+.LC1:
+	.string	"%d\t%d\n"
+	.text
+	.globl	printArrayWithFrequency
+	.type	printArrayWithFrequency, @function
+printArrayWithFrequency:
+.LFB1:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movq	%rdi, -24(%rbp)
+	movq	%rsi, -32(%rbp)
+	movl	%edx, -36(%rbp)
+	leaq	.LC0(%rip), %rdi
+	call	puts@PLT
+	movl	$0, -4(%rbp)
+	jmp	.L9
+.L11:
 	movl	-4(%rbp), %eax
 	cltq
 	leaq	0(,%rax,4), %rdx
-	movq	32(%rbp), %rax
+	movq	-32(%rbp), %rax
 	addq	%rdx, %rax
 	movl	(%rax), %eax
 	testl	%eax, %eax
@@ -127,176 +116,146 @@ calculateFrequency:
 	movl	-4(%rbp), %eax
 	cltq
 	leaq	0(,%rax,4), %rdx
-	movq	32(%rbp), %rax
-	addq	%rax, %rdx
-	movl	-12(%rbp), %eax
-	movl	%eax, (%rdx)
-.L10:
-	addl	$1, -4(%rbp)
-.L6:
-	movl	-4(%rbp), %eax
-	cmpl	24(%rbp), %eax
-	jl	.L11
-	nop
-	nop
-	addq	$16, %rsp
-	popq	%rbp
-	ret
-	.seh_endproc
-	.section .rdata,"dr"
-.LC0:
-	.ascii "Element\11Frequency\12\0"
-.LC1:
-	.ascii "%d\11%d\12\0"
-	.text
-	.globl	printArrayWithFrequency
-	.def	printArrayWithFrequency;	.scl	2;	.type	32;	.endef
-	.seh_proc	printArrayWithFrequency
-printArrayWithFrequency:
-	pushq	%rbp
-	.seh_pushreg	%rbp
-	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
-	subq	$48, %rsp
-	.seh_stackalloc	48
-	.seh_endprologue
-	movq	%rcx, 16(%rbp)
-	movq	%rdx, 24(%rbp)
-	movl	%r8d, 32(%rbp)
-	leaq	.LC0(%rip), %rax
-	movq	%rax, %rcx
-	call	printf
-	movl	$0, -4(%rbp)
-	jmp	.L13
-.L15:
-	movl	-4(%rbp), %eax
-	cltq
-	leaq	0(,%rax,4), %rdx
-	movq	24(%rbp), %rax
-	addq	%rdx, %rax
-	movl	(%rax), %eax
-	testl	%eax, %eax
-	je	.L14
-	movl	-4(%rbp), %eax
-	cltq
-	leaq	0(,%rax,4), %rdx
-	movq	24(%rbp), %rax
+	movq	-32(%rbp), %rax
 	addq	%rdx, %rax
 	movl	(%rax), %edx
 	movl	-4(%rbp), %eax
 	cltq
 	leaq	0(,%rax,4), %rcx
-	movq	16(%rbp), %rax
+	movq	-24(%rbp), %rax
 	addq	%rcx, %rax
 	movl	(%rax), %eax
-	movl	%edx, %r8d
-	movl	%eax, %edx
-	leaq	.LC1(%rip), %rax
-	movq	%rax, %rcx
-	call	printf
-.L14:
+	movl	%eax, %esi
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+.L10:
 	addl	$1, -4(%rbp)
-.L13:
+.L9:
 	movl	-4(%rbp), %eax
-	cmpl	32(%rbp), %eax
-	jl	.L15
+	cmpl	-36(%rbp), %eax
+	jl	.L11
 	nop
 	nop
-	addq	$48, %rsp
-	popq	%rbp
+	leave
+	.cfi_def_cfa 7, 8
 	ret
-	.seh_endproc
-	.def	__main;	.scl	2;	.type	32;	.endef
-	.section .rdata,"dr"
+	.cfi_endproc
+.LFE1:
+	.size	printArrayWithFrequency, .-printArrayWithFrequency
+	.section	.rodata
 	.align 8
 .LC2:
-	.ascii "\12\12Count frequency of each integer element of an array:\12\0"
+	.string	"\n\nCount frequency of each integer element of an array:"
 	.align 8
 .LC3:
-	.ascii "------------------------------------------------\12\0"
+	.string	"------------------------------------------------"
 	.align 8
 .LC4:
-	.ascii "Input the number of elements to be stored in the array :\0"
+	.string	"Input the number of elements to be stored in the array :"
 .LC5:
-	.ascii "%d\0"
+	.string	"%d"
 	.align 8
 .LC6:
-	.ascii "Enter each elements separated by space: \0"
+	.string	"Enter each elements separated by space: "
 	.text
 	.globl	main
-	.def	main;	.scl	2;	.type	32;	.endef
-	.seh_proc	main
+	.type	main, @function
 main:
+.LFB2:
+	.cfi_startproc
+	endbr64
 	pushq	%rbp
-	.seh_pushreg	%rbp
-	subq	$864, %rsp
-	.seh_stackalloc	864
-	leaq	128(%rsp), %rbp
-	.seh_setframe	%rbp, 128
-	.seh_endprologue
-	call	__main
-	leaq	.LC2(%rip), %rax
-	movq	%rax, %rcx
-	call	printf
-	leaq	.LC3(%rip), %rax
-	movq	%rax, %rcx
-	call	printf
-	leaq	.LC4(%rip), %rax
-	movq	%rax, %rcx
-	call	printf
-	leaq	-84(%rbp), %rax
-	movq	%rax, %rdx
-	leaq	.LC5(%rip), %rax
-	movq	%rax, %rcx
-	call	scanf
-	leaq	.LC6(%rip), %rax
-	movq	%rax, %rcx
-	call	printf
-	movl	$0, 732(%rbp)
-	jmp	.L17
-.L18:
-	leaq	320(%rbp), %rdx
-	movl	732(%rbp), %eax
-	cltq
-	salq	$2, %rax
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$832, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	.LC2(%rip), %rdi
+	call	puts@PLT
+	leaq	.LC3(%rip), %rdi
+	call	puts@PLT
+	leaq	.LC4(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-828(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC5(%rip), %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	leaq	.LC6(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, -824(%rbp)
+	jmp	.L13
+.L14:
+	leaq	-816(%rbp), %rax
+	movl	-824(%rbp), %edx
+	movslq	%edx, %rdx
+	salq	$2, %rdx
 	addq	%rdx, %rax
-	movq	%rax, %rdx
-	leaq	.LC5(%rip), %rax
-	movq	%rax, %rcx
-	call	scanf
-	addl	$1, 732(%rbp)
-.L17:
-	movl	-84(%rbp), %eax
-	cmpl	%eax, 732(%rbp)
-	jl	.L18
-	movl	$0, 728(%rbp)
-	jmp	.L19
-.L20:
-	movl	728(%rbp), %eax
+	movq	%rax, %rsi
+	leaq	.LC5(%rip), %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	addl	$1, -824(%rbp)
+.L13:
+	movl	-828(%rbp), %eax
+	cmpl	%eax, -824(%rbp)
+	jl	.L14
+	movl	$0, -820(%rbp)
+	jmp	.L15
+.L16:
+	movl	-820(%rbp), %eax
 	cltq
-	movl	$-1, -80(%rbp,%rax,4)
-	addl	$1, 728(%rbp)
-.L19:
-	movl	-84(%rbp), %eax
-	cmpl	%eax, 728(%rbp)
-	jl	.L20
-	movl	-84(%rbp), %edx
-	leaq	-80(%rbp), %rcx
-	leaq	320(%rbp), %rax
-	movq	%rcx, %r8
-	movq	%rax, %rcx
+	movl	$-1, -416(%rbp,%rax,4)
+	addl	$1, -820(%rbp)
+.L15:
+	movl	-828(%rbp), %eax
+	cmpl	%eax, -820(%rbp)
+	jl	.L16
+	movl	-828(%rbp), %ecx
+	leaq	-416(%rbp), %rdx
+	leaq	-816(%rbp), %rax
+	movl	%ecx, %esi
+	movq	%rax, %rdi
 	call	calculateFrequency
-	movl	-84(%rbp), %ecx
-	leaq	-80(%rbp), %rdx
-	leaq	320(%rbp), %rax
-	movl	%ecx, %r8d
-	movq	%rax, %rcx
+	movl	-828(%rbp), %edx
+	leaq	-416(%rbp), %rcx
+	leaq	-816(%rbp), %rax
+	movq	%rcx, %rsi
+	movq	%rax, %rdi
 	call	printArrayWithFrequency
 	movl	$0, %eax
-	addq	$864, %rsp
-	popq	%rbp
+	movq	-8(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L18
+	call	__stack_chk_fail@PLT
+.L18:
+	leave
+	.cfi_def_cfa 7, 8
 	ret
-	.seh_endproc
-	.ident	"GCC: (Rev10, Built by MSYS2 project) 11.2.0"
-	.def	__mingw_vfscanf;	.scl	2;	.type	32;	.endef
-	.def	__mingw_vfprintf;	.scl	2;	.type	32;	.endef
+	.cfi_endproc
+.LFE2:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
