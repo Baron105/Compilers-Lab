5596c908fd344587d930ea2addedb6ed41f81225
Baron105 <barunparua@gmail.com>
2023-08-22 23:05:18 +0530

    added main file as per lex file

---
+++ Assignment 3/ass3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+// macro definitions
+#define KEYWORD 1
+#define IDENTIFIER 2
+#define INTEGER_CONSTANT 3
+#define FLOATING_CONSTANT 4
+#define CHAR_CONSTANT 5
+#define STRING_LITERAL 6
+#define PUNCTUATOR 7
+
+extern char *yytext;
+extern int yylex();
+
+int main()
+{
+    int token;
+    while ((token = yylex()))
+    {
+        switch (token)
+        {
+        case KEYWORD:
+            printf("<KEYWORD, %d, %s>\n", token, yytext);
+            break;
+        case IDENTIFIER:
+            printf("<IDENTIFIER, %d, %s>\n", token, yytext);
+            break;
+        case INTEGER_CONSTANT:
+            printf("<INTEGER_CONSTANT, %d, %s>\n", token, yytext);
+            break;
+        case FLOATING_CONSTANT:
+            printf("<FLOATING_CONSTANT, %d, %s>\n", token, yytext);
+            break;
+        case CHAR_CONSTANT:
+            printf("<CHAR_CONSTANT, %d, %s>\n", token, yytext);
+            break;
+        case STRING_LITERAL:
+            printf("<STRING_LITERAL, %d, %s>\n", token, yytext);
+            break;
+        case PUNCTUATOR:
+            printf("<PUNCTUATOR, %d, %s>\n", token, yytext);
+            break;
+        default:
+            printf("<INVALID_TOKEN, %s>\n", yytext);
+            break;
+        }
+    }
+    return 0;
+}
