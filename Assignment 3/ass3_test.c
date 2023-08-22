eaf1cf4dfd43b41dae6a491cfc7d2da1a1721d99
Baron105 <barunparua@gmail.com>
2023-08-22 23:23:11 +0530

    test_file

---
+++ Assignment 3/ass3_test.c
@@ -0,0 +1,20 @@
+// test file for assignment 3
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int c;
+    c = a + b;
+    c = a - b;
+    c = a * b;
+    c = a / b;
+    c = a % b;
+    c = a++;
+    c = a--;
+    c = a & b;
+    c = a | b;
+    c = a ^ b;
+    c = a << b;
+    c = a >> b;
+}
