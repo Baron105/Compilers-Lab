1fb857280ed4f69e0b6c22b51b7273840a74a3e5
Baron105 <barunparua@gmail.com>
2023-08-25 20:48:44 +0530

    added functionality for comments

--- Assignment 3/ass3.c
+++ Assignment 3/ass3.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 // macro definitions
-#define KEYWORD 1
-#define IDENTIFIER 2
-#define INTEGER_CONSTANT 3
-#define FLOATING_CONSTANT 4
-#define CHAR_CONSTANT 5
-#define STRING_LITERAL 6
-#define PUNCTUATOR 7
+
+#define SINGLELINE_COMMENT 1
+#define MULTILINE_COMMENT 2
+#define KEYWORD 3
+#define IDENTIFIER 4
+#define INTEGER_CONSTANT 5
+#define FLOATING_CONSTANT 6
+#define CHAR_CONSTANT 7
+#define STRING_LITERAL 8
+#define PUNCTUATOR 9
 
 extern char *yytext;
 extern int yylex();
@@ -19,29 +22,35 @@ int main()
     {
         switch (token)
         {
+        case SINGLELINE_COMMENT:
+            printf("<SINGLELINE_COMMENT: %s , TOKEN: %d>\n", yytext, token);
+            break;
+        case MULTILINE_COMMENT:
+            printf("<MULTILINE_COMMENT: %s , TOKEN: %d>\n", yytext, token);
+            break;
         case KEYWORD:
-            printf("<KEYWORD, %d, %s>\n", token, yytext);
+            printf("<KEYWORD: %s , TOKEN: %d>\n", yytext, token);
             break;
         case IDENTIFIER:
-            printf("<IDENTIFIER, %d, %s>\n", token, yytext);
+            printf("<IDENTIFIER: %s , TOKEN: %d>\n", yytext, token);
             break;
         case INTEGER_CONSTANT:
-            printf("<INTEGER_CONSTANT, %d, %s>\n", token, yytext);
+            printf("<INTEGER_CONSTANT: %s , TOKEN: %d>\n", yytext, token);
             break;
         case FLOATING_CONSTANT:
-            printf("<FLOATING_CONSTANT, %d, %s>\n", token, yytext);
+            printf("<FLOATING_CONSTANT: %s , TOKEN: %d>\n", yytext, token);
             break;
         case CHAR_CONSTANT:
-            printf("<CHAR_CONSTANT, %d, %s>\n", token, yytext);
+            printf("<CHAR_CONSTANT: %s , TOKEN: %d>\n", yytext, token);
             break;
         case STRING_LITERAL:
-            printf("<STRING_LITERAL, %d, %s>\n", token, yytext);
+            printf("<STRING_LITERAL: %s , TOKEN: %d>\n", yytext, token);
             break;
         case PUNCTUATOR:
-            printf("<PUNCTUATOR, %d, %s>\n", token, yytext);
+            printf("<PUNCTUATOR: %s , TOKEN: %d>\n", yytext, token);
             break;
         default:
-            printf("<INVALID_TOKEN, %s>\n", yytext);
+            printf("<INVALID TOKEN: %s>\n", yytext);
             break;
         }
     }
