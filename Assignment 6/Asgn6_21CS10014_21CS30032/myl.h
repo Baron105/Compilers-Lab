7a191e136a268a1148cb6910a3550eeab2c7d7ad
Baron105 <barunparua@gmail.com>
2023-10-28 12:47:53 +0530

    myl and c files

---
+++ Assignment 6/Asgn6_21CS10014_21CS30032/myl.h
@@ -0,0 +1,10 @@
+#ifndef _MYL_H
+#define _MYL_H
+#define ERR 0
+#define OK 1
+int printStr(char *);
+int printInt(int);
+int readInt(int *eP); // Usual meaning as specified in Assignment #2
+int printFlt(float);
+int readFlt(float *eP); // Usual meaning as specified in Assignment #2
+#endif
